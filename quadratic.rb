b0805489a1c9f35c4e4e2e1f11f6e6575832bde7
Astor Pena <63073586+astorpena@users.noreply.github.com>
2022-02-25 12:18:16 -0500

    Ask for input

--- quadratic.rb
+++ quadratic.rb
@@ -1,8 +1,11 @@
-a=gets.chomp('What is a?').to_i
-b=gets.chomp('What is b?').to_i
-c=gets.chomp('What is c?').to_i
+print('What is a?')
+a=gets.chomp.to_i
+print('What is b?')
+b=gets.chomp.to_i
+print('What is c?')
+c=gets.chomp.to_i
 d=(b*b)-(4*a*c)
 if(d<0)
     abort('The discriminant is less than zero. There are no real solutions.')
 end
-print("x = (-#{b} \u00b1", " ", Math.sqrt(d),")\n     /", " ", 2 * a, "\n")
+print("x = (-#{b} \u00b1", " ", Math.sqrt(d), ")\n     /", " ", 2 * a, "\n")
